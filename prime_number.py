9f697d0f57c87a7f4452a6afe6175583306aa445
Chris <christopher.sewell@live.co.uk>
2014-03-19 20:15:40 +0000

    Re-written client
    Improved delegation logic and prime algorithm,

--- prime_number.py
+++ prime_number.py
@@ -18,7 +18,7 @@ def define(name,problem):
     
     if response.status_code != 200:
         response = requests.post('http://runtime.azurewebsites.net/runtime/define/'+ name,data=problem)
-        
+        print(response)
         
 def delegate(method,parameters):
     request_str = 'http://runtime.azurewebsites.net/runtime/' + method
@@ -30,57 +30,55 @@ def delegate(method,parameters):
     dom = xml.dom.minidom.parseString(response.text)
     xml_return = dom.getElementsByTagName('Return')
     print('CPU: ' + str(psutil.cpu_percent(interval=1)) + '%' + ' DELEGATED at ' + str(datetime.now().isoformat()) + ' ' + str(xml_return[0].firstChild.nodeValue)) 
-    return int(xml_return[0].firstChild.nodeValue)    
+    return float(xml_return[0].firstChild.nodeValue)    
  
 def is_prime(n):
-   
+    print('Testing '  + str(n))
     '''Is n integer ?'''
     n = abs(int(n))
     '''Is n less than 2 ? '''
     if n < 2:
         return 0
+    
     '''Is n 2 ? '''
     if n == 2:
         return 1
     
     '''Is n even ?'''   
-    if psutil.cpu_percent(interval=1) >= 50:
-        is_even = delegate("Modulus",[n,2])       
-    else:
-        is_even = n % 2
-        print('CPU: ' + str(psutil.cpu_percent(interval=1)) + '%' + ' LOCAL')
-        
-    if is_even == 0:
+    if 0 == (n % 2):
         return 0
-        
-    '''Create number range starting at 3 iterating 2 to square root of n!'''
+
+    '''Create number range starting at 3 iterating 2 to square root of n + 1!'''
     if psutil.cpu_percent(interval=1) >= 50:
-        n_sqrt = delegate("PlusSquare",[1,n])
-    else:
-        n_sqrt = int( math.sqrt(n) + 1)
-        print('CPU: ' + str(psutil.cpu_percent(interval=1)) + '%' + ' LOCAL')
-
-    for x in range(3,n_sqrt, 2):
-        '''Is n modulus x possible ?'''
-        if psutil.cpu_percent(interval=1) >= 50:
-            prime = delegate("Modulus",[n_sqrt,x])
-        else:
-            prime = n_sqrt % x
-            print('CPU: ' + str(psutil.cpu_percent(interval=1)) + '%' + ' LOCAL') 
-
-        if prime == 0:  
+        n_squared = int(delegate("PlusSquare",[1,n]))
+        prime = delegate("Loop",["Or",3,n_squared,2,"Modulus",n,"i"])
+        if prime != 1:
             return 0
-        
-    print (n)
+
+    else:
+        print('CPU: ' + str(psutil.cpu_percent(interval=1)) + '%' + ' LOCAL')
+        n_squared = int(math.sqrt(n) + 1)
+   
+        for x in range(3,n_squared, 2):
+            '''Is n modulus x possible ?'''
+            prime = n % x
+            if prime == 0:
+                return 0
+
     return 1
  
  
-numbers_to_find = 10000
+numbers_to_find = 5
 numbers_found = 0
-
+n = 0
 define("PlusSquare",open('PlusSquare.xml','r').read())
 
-for n in range(0, numbers_to_find):
-    numbers_found += is_prime(n)
-    
-print (numbers_found)
+
+while numbers_found < numbers_to_find:
+    n += 1
+    result = is_prime(n)
+    if result == 1:
+        print(str(n) + ' is a prime!')
+        numbers_found += result
+          
+print(numbers_found)
